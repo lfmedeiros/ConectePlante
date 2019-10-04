baabe5321104e55b054271a5407810be072e751f
lfmedeiros <lucamede@gmail.com>
2019-10-04 14:29:26 -0300

    Create data.js

--- server.js
+++ server.js
@@ -1,38 +1,24 @@
-const { GraphQLServer } = require('graphql-yoga');
+const {GraphQLServer} = require('graphql-yoga');
+const data = require('./data'); // Instanciar o arquivo data
 
-const typeDefs = ` 
 
+const typeDefs = `
 type Channel {
-    idChannel: Int,
-    name: String,
-    playlists: [PLaylist]
-}
-
-type Playlist{
-    idPlaylist: Int,
     idChannel: Int,
     name: String
 }
-
 type Query{
-    channels:([idChannel:int]): [Channel]
+    channels:[Channel]
 }
-
 `;
 const resolvers = {
     Query: {
-        channels(obj, args) {
-            return data.getData('channels', 'idChannel', args.idChannel);
-        }
+        channels() {
+            return data.getData('channels');
 
-    },
-    Channel: {
-        playlists: function (obj, args) {
-            return data.getData('playlists', 'idChannel', obj.idChannel);
         }
     }
-};
-
+}
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start();
