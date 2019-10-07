59bcfe54ed5e31f4b6c8d08c1322a6bb97a88651
Lucas Medeiros <50759691+lfmedeiros@users.noreply.github.com>
2019-10-06 22:44:21 -0300

    Delete database.js

--- database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const express = require('express');
-
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017/graphql', {useNewUrlParser: true});
